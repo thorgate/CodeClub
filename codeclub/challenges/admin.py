7e0afb4425eb5e073f80d2643c849967723b99e0
Taavi Teska <taavi@thorgate.eu>
2016-02-17 18:45:05 +0200

    Add filters and search for solutions in admin

--- codeclub/challenges/admin.py
+++ codeclub/challenges/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from challenges.models import Event, Challenge, Solution
 
 
+class SolutionAdmin(admin.ModelAdmin):
+    search_fields = ['user__email', 'user__name']
+    list_filter = ('challenge__title', 'status')
+
 admin.site.register(Event)
 admin.site.register(Challenge)
-admin.site.register(Solution)
+admin.site.register(Solution, SolutionAdmin)
