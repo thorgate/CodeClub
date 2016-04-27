0077fae78135a59f55268c11335b165467f64567
Romet Tagobert <sgzfxn@gmail.com>
2016-04-27 14:11:56 +0300

    merged migrations, filter out sizeless solutions in golf

--- codeclub/challenges/migrations/0011_auto_20160427_1410.py
+++ codeclub/challenges/migrations/0011_auto_20160427_1410.py
@@ -7,10 +7,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('challenges', '0011_challenge_golf'),
+        ('challenges', '0010_auto_20160218_2251'),
     ]
 
     operations = [
+        migrations.AddField(
+            model_name='challenge',
+            name='golf',
+            field=models.BooleanField(default=False),
+        ),
         migrations.AddField(
             model_name='solution',
             name='solution_size',
--- codeclub/challenges/views.py
+++ codeclub/challenges/views.py
@@ -138,13 +138,15 @@ class ChallengeJSON(ProtectedMixin, BaseDetailView):
         solutions = self.object.solution_set.filter(user=self.request.user).order_by('-timestamp')
 
         order_columns = ['timestamp']
+        filters = {'status': Solution.STATUS_CORRECT}
+
         if self.object.golf:
             order_columns.insert(0, 'solution_size')
+            filters['solution_size__isnull'] = False
 
         # We could use DISTINCT ON user_id, but it only works in PostgreSQL
-        all_correct_solutions = self.object.solution_set.filter(
-            status=Solution.STATUS_CORRECT,
-        ).order_by(*order_columns).select_related('user')
+        all_correct_solutions = self.object.solution_set\
+            .filter(**filters).order_by(*order_columns).select_related('user')
 
         # Do the mapping in memory so that we have the correct order of the users
         # Cache is used for detecting if we have already added the user to the list or not
