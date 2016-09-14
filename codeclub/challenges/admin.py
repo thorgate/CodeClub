709fdb88e006a97a291e7ab8de04d94bf6b02790
Taavi Teska <taavi@thorgate.eu>
2016-09-14 15:38:37 +0300

    Improve Event and Challenge admin model

--- codeclub/challenges/admin.py
+++ codeclub/challenges/admin.py
@@ -3,10 +3,23 @@ from django.contrib import admin
 from challenges.models import Event, Challenge, Solution
 
 
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+    list_filter = ('public',)
+    list_display = ('title', 'public')
+
+
+class ChallengeAdmin(admin.ModelAdmin):
+    search_fields = ['event__title']
+    list_filter = ('event', 'event__public', 'public', 'author', 'golf')
+    list_display = ('title', 'public', 'event', 'author', 'golf')
+
+
 class SolutionAdmin(admin.ModelAdmin):
     search_fields = ['user__email', 'user__name']
     list_filter = ('challenge__title', 'status')
 
-admin.site.register(Event)
-admin.site.register(Challenge)
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Challenge, ChallengeAdmin)
 admin.site.register(Solution, SolutionAdmin)
